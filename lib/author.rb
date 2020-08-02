1922641999ea5e66fb5f754a1fdaa6243512503b
Desiree Marquez <marquezdecardona@gmail.com>
2020-08-02 21:46:40 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -12,31 +12,22 @@
   def self.all
     @@all
   end
-  
+
+  def add_post(post)
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
+  end
+
   def posts
     Post.all.select do |post| 
       post.author == self
   end
-end
 
-  def add_post(post)
-    post.author = self
-    
-end
-  
-  def add_post_by_title(title)
-    post = Post.new(title)
-    post.author == self
+  def self.post_count
+    Post.all.count
   end
-  
-
 end
-    
-    # #describe "#posts" do
-    #   it "has many posts" do
-    #     expect(betty.posts).to be_a(Array)
-    #     post = Post.new("My Post")
-    #     post.author = betty
-    #     expect(betty.posts).to eq([post])
-    #   end
-    

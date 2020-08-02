5193129bfd4cf8e32e4062aaf893fe0d11701362
Desiree Marquez <marquezdecardona@gmail.com>
2020-08-02 21:34:17 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,38 @@
+
+class Author 
+  attr_accessor :name, :posts
+  
+  def initialize(name)
+    @name=name
+ 
+  end
+  
+  def posts
+    Post.all.select do |post| 
+      post.author == self
+  end
+end
+
+  def add_post(post)
+    post.author = self
+    
+end
+
+  def add_post_by_title(post_title) 
+   singer= Post.new(post_title)
+   add_post(singer)
+ end
+  
+  
+  
+
+end
+    
+    # #describe "#posts" do
+    #   it "has many posts" do
+    #     expect(betty.posts).to be_a(Array)
+    #     post = Post.new("My Post")
+    #     post.author = betty
+    #     expect(betty.posts).to eq([post])
+    #   end
+    
